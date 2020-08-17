069d23e8907dd40fcc646cc7edce3a7af2555cae
ajudge026 <67866986+ajudge026@users.noreply.github.com>
2020-08-17 07:18:52 -0400

    Add files via upload

--- Core.c
+++ Core.c
@@ -124,8 +124,8 @@ bool tickFunc(Core *core)
 		//printf("%s = %ld\n",VariableName(M_reg_load.signals.RegWrite),M_reg_load.signals.RegWrite);
 		//printf("%s = %ld\n",VariableName(core->WB_reg.reg_write_mux_val),core->WB_reg.reg_write_mux_val);
 	}
-	printf("Stages complete = %d#####################################################\n",core->stages_complete);	
-	printf("Sol = $$$$$$$$$$$$$$$$$$$!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+	printf("Stages complete = %d\n",core->stages_complete);	
+	printf("REG FILES\n");
 	printf("%s = %ld\n",VariableName(core->reg_file[10]),core->reg_file[10] );
 	printf("%s = %ld\n",VariableName(core->reg_file[11]),core->reg_file[11] );
 	printf("%s = %ld\n",VariableName(core->reg_file[12]),core->reg_file[12] );
